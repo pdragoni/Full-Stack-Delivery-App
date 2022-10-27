b3fc7f04677d14e42bfb719b08be82871b599536
pdragoni <paulohbfdf@gmail.com>
2022-10-27 17:22:12 -0300

    feat: Página de login com validações e com importação em App.js conforme Readme.MD

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
+        <Route exact path="/login" element={ <Login /> } />
+        {/* <Route exact path="/register" element={ <Login /> } /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,74 @@
+import { React, useState, useEffect } from 'react';
+
+export default function Login() {
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [validEmail, setValidEmail] = useState(true);
+
+  const onInputChange = ({ target }) => {
+    if (target.name === 'email') {
+      setEmail(target.value);
+    } else {
+      setPassword(target.value);
+    }
+  };
+
+  useEffect(() => {
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const minPasswordLength = 6;
+    if (regex.test(email) && password.length >= minPasswordLength) {
+      setBtnDisable(false);
+      // setValidEmail(true);
+    } else {
+      setBtnDisable(true);
+      // setValidEmail(false);
+    }
+  }, [email, password, btnDisable]);
+
+  return (
+    <div>
+      <label htmlFor="email">
+        Insira seu e-mail
+        <input
+          data-testid="common_login__input-email"
+          type="email"
+          name="email"
+          placeholder="user@email.com"
+          onChange={ onInputChange }
+        />
+      </label>
+      <br />
+      <label htmlFor="password">
+        Insira sua senha
+        <input
+          data-testid="common_login__input-password"
+          type="password"
+          name="password"
+          placeholder="******"
+          onChange={ onInputChange }
+        />
+      </label>
+      <button
+        data-testid="common_login__input-login"
+        type="button"
+        disabled={ btnDisable }
+        onClick={ () => console.log('Login com sucesso') }
+      >
+        Login
+      </button>
+      <button
+        data-testid="common_login__input-login"
+        type="button"
+        disabled={ btnDisable }
+        onClick={ () => console.log('Login com sucesso') }
+      >
+        Cadastrar
+      </button>
+      {/* {
+        !validEmail
+        && <span data-testid="common_login__element-invalid-email">E-mail inválido</span>
+      } */}
+    </div>
+  );
+}
