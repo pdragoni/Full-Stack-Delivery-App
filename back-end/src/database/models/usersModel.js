6b1e8871d6be31d3643824e0cf7c874d2890e160
Igor Castro <ncastro.igor@gmail.com>
2022-10-27 17:06:38 -0400

    feat: cria models, seeders e migrations para a tabela users

---
+++ back-end/src/database/models/usersModel.js
@@ -0,0 +1,23 @@
+const Users = (sequelize, DataTypes) => {
+  const Users = sequelize.define('users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users'
+  });
+
+  Users.associate = (models) => {
+    Users.hasMany(models.Sales,
+      { foreignKey: 'user_id', as: 'users' },
+      { foreignKey: 'seller_id', as: 'users' })
+  }
+
+  return Users;
+}
+
+module.exports = Users;
