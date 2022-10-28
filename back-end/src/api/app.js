b7b817526911b9641e21cb98d069bc8954739a58
pdragoni <paulohbfdf@gmail.com>
2022-10-28 19:45:05 -0300

    fix: Lint em backend/src/app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const Route = require('./routes');
-const cors = require ('cors');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
