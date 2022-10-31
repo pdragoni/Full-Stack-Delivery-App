08a8bd2b4b09690812c9426ec4bc9770be49d3a3
pdragoni <paulohbfdf@gmail.com>
2022-10-31 17:17:51 -0300

    feat: implementa chave 'quantity' em cada um dos produtos. Botão adicionar implementado

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -5,11 +5,26 @@ import { getProducts } from '../API/instance';
 export default function Products() {
   const [products, setProducts] = useState([]);
 
+  const adicionaItem = (id) => {
+    console.log(id);
+    const grupo21 = products.map((product) => {
+      if (product.id - 1 === id) {
+        let { quantity } = product;
+        quantity += 1;
+        return { ...product, quantity };
+      }
+      return product;
+    });
+    console.log(grupo21);
+    setProducts(grupo21);
+  };
+
   useEffect(() => {
     const allProducts = async () => {
       const produtos = await getProducts();
-      console.log(produtos);
-      return setProducts(produtos);
+      const g21 = produtos.map((prod) => ({ ...prod, quantity: 0 }));
+      console.log(g21);
+      return setProducts(g21);
     };
     allProducts();
   }, []);
@@ -19,11 +34,28 @@ export default function Products() {
       <Navbar />
       {
         products.map((product) => (
-          <div
+          <fieldset
+            data-testid={ `customer_products__element-card-price-${product.id}` }
             key={ product.id }
           >
             <p>{ product.name }</p>
-          </div>
+            <p>{ product.price }</p>
+            <img src={ product.url_image } alt={ product.name } />
+            <button
+              type="button"
+              onClick={ () => adicionaItem(product.id) }
+            >
+              Adicionar
+            </button>
+            <button
+              type="button"
+              onClick={ () => console.log('remover') }
+            >
+              Remover
+
+            </button>
+
+          </fieldset>
         ))
       }
     </div>
