0cd8463fad783434b22af0cd960faafcc4338c42
pdragoni <paulohbfdf@gmail.com>
2022-10-31 19:27:19 -0300

    feat: implementa elementos em NavBar. Botão Logout já funcionando

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,7 +1,47 @@
 import { React } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const logoutClick = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
-    <h1>NavBar</h1>
+    <header>
+      <button
+        data-testid="customer_products__element-navbar-link-products"
+        type="button"
+        onClick={ () => navigate('/products') }
+      >
+        Produtos
+      </button>
+      <button
+        data-testid="customer_products__element-navbar-link-orders"
+        type="button"
+        onClick={ () => navigate('/orders') }
+      >
+        Pedidos
+
+      </button>
+      <button
+        data-testid="customer_products__element-navbar-user-full-name"
+        type="button"
+        onClick={ () => navigate('/profile') }
+      >
+        Perfil
+
+      </button>
+      <button
+        data-testid="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ logoutClick }
+      >
+        Sair
+
+      </button>
+    </header>
   );
 }
