d73b8ecf94e30ae743c10ccc887fc041bdb6ada0
pdragoni <paulohbfdf@gmail.com>
2022-11-01 20:20:25 -0300

    feat: recebe página Checkout em App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,18 +5,21 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
 import Checkout from './pages/Checkout';
+import Provider from './API/Provider';
 
 function App() {
   return (
 
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={ <Navigate to="/login" /> } />
-        <Route exact path="/login" element={ <Login /> } />
-        <Route exact path="/register" element={ <Register /> } />
-        <Route exact path="/customer/products" element={ <Products /> } />
-        <Route exact path="/customer/checkout" element={ <Checkout /> } />
-      </Routes>
+      <Provider>
+        <Routes>
+          <Route exact path="/" element={ <Navigate to="/login" /> } />
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/register" element={ <Register /> } />
+          <Route exact path="/customer/products" element={ <Products /> } />
+          <Route exact path="/customer/checkout" element={ <Checkout /> } />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
