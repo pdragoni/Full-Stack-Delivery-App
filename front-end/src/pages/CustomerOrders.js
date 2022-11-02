c1bbd3f78ea98703ce87ea539a830945f58e6fe5
LDeive505 <luisdaviab@gmail.com>
2022-11-02 17:13:35 -0300

    feat: cria pagina de pedidos e adiciona rota em App.js

---
+++ front-end/src/pages/CustomerOrders.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import Navbar from '../components/Navbar';
+
+export default function CustomerOrders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    
+  }, []);
+  return (
+    <div>
+      <Navbar />
+
+    </div>
+  );
+}
