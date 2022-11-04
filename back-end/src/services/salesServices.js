911dcf81e6104e59b002784b3cd15e9293f73d54
Igor Castro <ncastro.igor@gmail.com>
2022-11-04 14:59:18 -0400

    fix: altera retorno de API do endpoint /sales/user

--- back-end/src/services/salesServices.js
+++ back-end/src/services/salesServices.js
@@ -1,10 +1,12 @@
-const { sales, products, salesProducts, sequelize } = require('../database/models');
+const { sales, users, products, salesProducts, sequelize } = require('../database/models');
 const { getByEmail } = require('./usersServices');
 
 const getAll = async () => {
   const allSales = await sales.findAll({
     include: [
       { model: products, as: 'products' },
+      { model: users, as: 'user' },
+      { model: users, as: 'seller' },
     ],
   });
   return allSales;
@@ -32,6 +34,8 @@ const findOne = async (id) => {
   const sale = await sales.findByPk(id, {
     include: [
       { model: products, as: 'products' },
+      { model: users, as: 'user' },
+      { model: users, as: 'seller' },
     ],
   });
   if (!sale) return null;
@@ -46,16 +50,32 @@ const update = async ({ id, status }) => {
   return updatedSale;
 };
 
-const findByUser = async (email) => {  
-  const user = await getByEmail(email);
-  if (!user) return null;
-
-  const { id: userId } = user;
-  const salesByUser = await sales.findAll({ where: { userId },
+const findSalesCustomer = async (id) => {
+  const result = await sales.findAll({ where: { userId: id },
     include: [
       { model: products, as: 'products' },
+      { model: users, as: 'seller' },
     ],
   });
+  return result;
+};
+
+const findSalesSeller = async (id) => {
+  const result = await sales.findAll({ where: { sellerId: id },
+    include: [
+      { model: products, as: 'products' },
+      { model: users, as: 'user' },
+    ],
+  });
+  return result;
+};
+
+const findByUser = async (email) => {  
+  const user = await getByEmail(email);
+  if (!user || user.role === 'administrator') return null;
+
+  const { id } = user;
+  const salesByUser = user.role === 'customer' ? await findSalesCustomer(id) : await findSalesSeller(id);
 
   if (salesByUser.length === 0 || !salesByUser) return null;
   return salesByUser;
