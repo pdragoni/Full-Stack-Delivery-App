c44042d6c4c5503ab49c62d15a253799c919d781
pdragoni <paulohbfdf@gmail.com>
2022-11-04 14:36:15 -0300

    feat: data-testids em OrderDetails. Precisa refactorar a exibição dos elementos HTML

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -6,15 +6,90 @@ import { getOrderById } from '../API/instance';
 export default function OrderDetails() {
   const [order, setOrder] = useState();
   const { id } = useParams();
-  // setOrder(getOrderById(id));
+
+  const string = 'customer_order_details__element-order-';
 
   useEffect(() => {
-    setOrder(getOrderById(id));
-  }, [id]);
+    getOrderById(id).then((response) => setOrder(response));
+    console.log(order);
+  }, []);
+
+  console.log(order);
 
   return (
     <div>
       <Navbar />
+      {
+        order && (
+          <div>
+            <span
+              data-testid={ `${string}details-label-order-id` }
+            >
+              { order.id }
+            </span>
+            <span
+              data-testid={ `${string}details-label-seller-name` }
+
+            >
+              { order.sellerId }
+            </span>
+            <span
+              data-testid={ `${string}details-label-order-date` }
+
+            >
+              { `Data: ${order.saleDate}` }
+            </span>
+            <span
+              data-testid={ `${string}details-label-delivery-status` }
+            />
+            <button
+              type="button"
+              onClick={ () => console.log('Pedido finalizado') }
+              data-testid="customer_order_details__button-delivery-check"
+            >
+              Marcar como entregue
+            </button>
+            {
+              order.products?.map((product, index) => (
+                <div
+                  key={ index }
+                >
+                  <span
+                    data-testid={ `${string}table-item-number-${index}` }
+                  >
+                    { (index + 1) }
+                  </span>
+                  <span
+                    data-testid={ `${string}table-name-${index}` }
+                  >
+                    { product.name }
+                  </span>
+                  <span
+                    data-testid={ `${string}table-quantity-${index}` }
+                  >
+                    { product.salesProducts.quantity }
+                  </span>
+                  <span
+                    data-testid={ `${string}table-unit-price-${index}` }
+                  >
+                    { product.price }
+                  </span>
+                  <span
+                    data-testid={ `${string}table-sub-total-${index}` }
+                  >
+                    { product.price * product.salesProducts.quantity }
+                  </span>
+                </div>
+              ))
+            }
+            <span
+              data-testid={ `${string}total-price` }
+            >
+              { order.totalPrice }
+            </span>
+          </div>
+        )
+      }
       <button type="button" onClick={ () => console.log(order) }>botaum</button>
     </div>
   );
