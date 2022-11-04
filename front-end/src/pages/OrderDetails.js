76c3f994d7742714557bef40879c9ac59b8c606a
pdragoni <paulohbfdf@gmail.com>
2022-11-04 13:34:45 -0300

    função setOrder para estado global, onde está o pedido por ID

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { getOrderById } from '../API/instance';
 
 export default function OrderDetails() {
+  const [order, setOrder] = useState();
+  const { id } = useParams();
+  // setOrder(getOrderById(id));
+
+  useEffect(() => {
+    setOrder(getOrderById(id));
+  }, [id]);
+
   return (
     <div>
       <Navbar />
+      <button type="button" onClick={ () => console.log(order) }>botaum</button>
     </div>
   );
 }
