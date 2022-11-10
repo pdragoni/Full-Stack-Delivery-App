c32a030287dec7810d70b61ea70664c99fc9a470
Igor Castro <ncastro.igor@gmail.com>
2022-11-10 19:37:19 -0400

    feat: importação do bootstrap

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
