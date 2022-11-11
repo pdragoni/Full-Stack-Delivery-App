23878d886931b3d87123cb94ecc7d6906a5bc0f0
Igor Castro <ncastro.igor@gmail.com>
2022-11-11 18:56:54 -0400

    feat: altera componente Navbar para estilização

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getLocalStorage } from '../helpers/localStorage';
+import navLogo from '../images/logo-alt-02.png';
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -11,35 +12,59 @@ export default function Navbar() {
   };
 
   return (
-    <header>
-      <button
-        data-testid="customer_products__element-navbar-link-products"
-        type="button"
-        onClick={ () => navigate('/customer/products') }
-      >
-        Produtos
-      </button>
-      <button
-        data-testid="customer_products__element-navbar-link-orders"
-        type="button"
-        onClick={ () => navigate(`/${role}/orders`) }
-      >
-        Pedidos
-      </button>
-      <button
-        data-testid="customer_products__element-navbar-user-full-name"
-        type="button"
-        onClick={ () => navigate('/profile') }
-      >
-        { name }
-      </button>
-      <button
-        data-testid="customer_products__element-navbar-link-logout"
-        type="button"
-        onClick={ logoutClick }
-      >
-        Sair
-      </button>
+    <header className="nav-header">
+      <div>
+        { role === 'customer' ? (
+          <button
+            className="nav-button"
+            data-testid="customer_products__element-navbar-link-products"
+            type="button"
+            onClick={ () => navigate('/customer/products') }
+          >
+            Produtos
+          </button>
+        ) : (
+          <span />
+        ) }
+        { role === 'administrator' ? (
+          <button
+            className="nav-button"
+            data-testid="customer_products__element-navbar-link-orders"
+            type="button"
+            onClick={ () => navigate('/admin/manage') }
+          >
+            Gerenciar usuários
+          </button>
+        ) : (
+          <button
+            className="nav-button"
+            data-testid="customer_products__element-navbar-link-orders"
+            type="button"
+            onClick={ () => navigate('/seller/orders') }
+          >
+            Pedidos
+          </button>
+        )}
+      </div>
+      <img src={ navLogo } alt="Logo da 21 Express" className="nav-logo" />
+      <div>
+        <button
+          className="nav-button"
+          data-testid="customer_products__element-navbar-user-full-name"
+          type="button"
+          onClick={ () => navigate('/profile') }
+        >
+          { name }
+        </button>
+        <button
+          className="nav-button-exit"
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+          onClick={ logoutClick }
+        >
+          Sair
+        </button>
+      </div>
     </header>
   );
 }
