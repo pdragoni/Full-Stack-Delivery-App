4721cf1b0b25682e96a96e8c8f63b30d2e5d3b3f
LDeive505 <luisdaviab@gmail.com>
2022-11-14 20:35:23 -0300

    refactor: refator o formulario da página em um componente

--- front-end/src/pages/AdminPage.js
+++ front-end/src/pages/AdminPage.js
@@ -1,109 +1,15 @@
-import React, { useState } from 'react';
-import { createUserByAdm } from '../API/instance';
+import React, { useState, useEf } from 'react';
 import Navbar from '../components/Navbar';
-import { getLocalStorage } from '../helpers/localStorage';
+import AdminRegisterForm from '../components/AdminRegisterForm';
 
 export default function AdminPage() {
-  const [formValues, setFormValues] = useState({
-    name: '',
-    email: '',
-    password: '',
-    role: 'seller',
-  });
-  const [showMessage, setShowMessage] = useState(false);
-
-  const onInputChange = ({ target: { name, value } }) => {
-    setFormValues((state) => ({ ...state, [name]: value }));
-  };
-
-  const enableButton = () => {
-    const { name, email, password } = formValues;
-    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    const six = 6;
-    const twelve = 12;
-    return !(password.length >= six && name.length >= twelve && regex.test(email));
-  };
-
-  const createUser = async () => {
-    const { name, email, password, role } = formValues;
-    const { token } = getLocalStorage('user');
-    const data = await createUserByAdm({ name, email, password, role }, token);
-    console.log(data);
-    if (data) {
-      console.log('Usuário criado com sucesso!');
-      return setFormValues({ name: '', email: '', password: '', role: 'seller' });
-    }
-    return setShowMessage(true);
-  };
+  // const [users, setUsers] = useState([]);
 
   return (
     <div>
       <Navbar />
-      <h2>Cadastrar novo usuário</h2>
-      <form>
-        <label htmlFor="name">
-          Nome
-          <input
-            data-testid="admin_manage__input-name"
-            type="text"
-            name="name"
-            value={ formValues.name }
-            placeholder="Nome e sobrenome"
-            onChange={ onInputChange }
-          />
-        </label>
-        <br />
-        <label htmlFor="email">
-          E-mail
-          <input
-            data-testid="admin_manage__input-email"
-            type="email"
-            name="email"
-            value={ formValues.email }
-            placeholder="user@email.com"
-            onChange={ onInputChange }
-          />
-        </label>
-        <br />
-        <label htmlFor="password">
-          Senha
-          <input
-            data-testid="admin_manage__input-password"
-            type="password"
-            name="password"
-            placeholder="******"
-            value={ formValues.password }
-            onChange={ onInputChange }
-          />
-        </label>
-        <br />
-        <label htmlFor="role">
-          Tipo
-          <select
-            data-testid="admin_manage__select-role"
-            name="role"
-            onChange={ onInputChange }
-            value={ formValues.role }
-          >
-            <option value="seller">Vendedor</option>
-            <option value="customer">Cliente</option>
-            <option value="administrator">Administrador</option>
-          </select>
-        </label>
-        <br />
-        <br />
-        <button
-          data-testid="admin_manage__button-register"
-          type="button"
-          disabled={ enableButton() }
-          onClick={ createUser }
-        >
-          Cadastrar
-        </button>
-      </form>
+      <AdminRegisterForm />
 
-      { showMessage
-      && <p data-testid="admin_manage__element-invalid-register">Usuário já existe</p> }
     </div>
   );
 }
